57f473636719ff8d666532b310e94df7521790cb
ashutosh <iamdeveloper.me@gmail.com>
2020-10-26 15:09:35 +0530

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as hostIds from '../../assets/jsons/host_ids.json';
+
+declare var $: any;
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+	hostIdsData: any;
   constructor() { }
 
   ngOnInit(): void {
+  	let data: {id: string}[]	= (hostIds as any).default;
+  	this.hostIdsData = data;
   }
-
 }
